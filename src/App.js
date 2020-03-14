da1d3f57fee705681822e62eb86dcc62d707f111
kec <57247368+yesky85@users.noreply.github.com>
2020-03-14 18:58:16 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from "react";
 
-function Food({ favourite }) {
-  return <h3>I like {favourite}</h3>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="meat" />
-      <Food favourite="fish" />
+      {foodILike.map(food => (
+        <Food name={food.name} picture={food.image} />
+      ))}
     </div>
   );
 }
