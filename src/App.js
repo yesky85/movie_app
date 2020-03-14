54a638e5d1f05eb8936705d7aebc77b441b7101e
kec <57247368+yesky85@users.noreply.github.com>
2020-03-14 14:06:20 +0900

    Add Base component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Base from "./Base";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Base />
     </div>
   );
 }
---
+++ src/Base.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Base() {
+  return <h3>React is good!</h3>;
+}
+
+export default Base;
